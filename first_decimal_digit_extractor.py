22cc357620a2e3b3d0a7a2cecc345a0482bbf407
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 22:16:26 +0300

    Create first_decimal_digit_extractor.py
    This program takes a positive real number as input and extracts the first digit after the decimal point.

---
+++ first_decimal_digit_extractor.py
@@ -0,0 +1,5 @@
+X = float(input("Enter a positive real number: "))
+
+first_number_after_decimal = int((X * 10) % 10)
+
+print("The first digit after the decimal point:", first_number_after_decimal)
